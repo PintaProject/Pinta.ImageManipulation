056a06a4179606a7babc1baad39fa27bf9452932
Jonathan Pobst <monkey@jpobst.com>
2012-12-25 15:16:25 -0600

    Add license header.

--- Pinta.ImageManipulation.Cairo/CairoSurfaceWrapper.cs
+++ Pinta.ImageManipulation.Cairo/CairoSurfaceWrapper.cs
@@ -1,25 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// 
+// CairoSurfaceWrapper.cs
+//  
+// Author:
+//       Jonathan Pobst <monkey@jpobst.com>
+// 
+// Copyright (c) 2012 Jonathan Pobst
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
 
 namespace Pinta.ImageManipulation
 {
 	public class CairoSurfaceWrapper : BaseSurface
 	{
 		private Cairo.ImageSurface surface;
-		private unsafe ImageManipulation.ColorBgra* data_ptr;
+		private unsafe ColorBgra* data_ptr;
 		private int lock_count = 0;
 
 		public unsafe CairoSurfaceWrapper (Cairo.ImageSurface surface)
 		{
 			this.surface = surface;
-			this.data_ptr = (ImageManipulation.ColorBgra*)surface.DataPtr;
+			this.data_ptr = (ColorBgra*)surface.DataPtr;
 			height = surface.Height;
 			width = surface.Width;
 		}
 
-		protected unsafe override ImageManipulation.ColorBgra* data {
+		protected unsafe override ColorBgra* data {
 			get { return data_ptr; }
 		}
 
--- Pinta.ImageManipulation.System.Drawing/BitmapWrapper.cs
+++ Pinta.ImageManipulation.System.Drawing/BitmapWrapper.cs
@@ -1,7 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// 
+// BitmapWrapper.cs
+//  
+// Author:
+//       Jonathan Pobst <monkey@jpobst.com>
+// 
+// Copyright (c) 2012 Jonathan Pobst
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
 using System.Drawing.Imaging;
 
 namespace Pinta.ImageManipulation
@@ -10,7 +33,7 @@ namespace Pinta.ImageManipulation
 	{
 		private System.Drawing.Bitmap surface;
 		private BitmapData bitmap_data;
-		private unsafe ImageManipulation.ColorBgra* data_ptr;
+		private unsafe ColorBgra* data_ptr;
 		private int lock_count = 0;
 
 		public unsafe BitmapWrapper (System.Drawing.Bitmap surface)
@@ -20,7 +43,7 @@ namespace Pinta.ImageManipulation
 			width = surface.Width;
 		}
 
-		protected unsafe override ImageManipulation.ColorBgra* data {
+		protected unsafe override ColorBgra* data {
 			get { return data_ptr; }
 		}
 
@@ -36,7 +59,7 @@ namespace Pinta.ImageManipulation
 				return;
 
 			bitmap_data = surface.LockBits (new System.Drawing.Rectangle (0, 0, surface.Width, surface.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
-			data_ptr = (ImageManipulation.ColorBgra*)bitmap_data.Scan0;
+			data_ptr = (ColorBgra*)bitmap_data.Scan0;
 		}
 
 		public override void EndUpdate ()
