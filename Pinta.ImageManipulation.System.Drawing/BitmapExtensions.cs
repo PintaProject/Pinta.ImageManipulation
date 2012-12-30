0146ab3d4d3f67288da1971cef3bb664942885c4
Jonathan Pobst <monkey@jpobst.com>
2012-12-30 12:57:44 -0600

    Add extension overload of Render that takes care of the BitmapWrapper.

---
+++ Pinta.ImageManipulation.System.Drawing/BitmapExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Pinta.ImageManipulation
+{
+	public static class BitmapExtensions
+	{
+		public static void Render (this BaseEffect effect, Bitmap source)
+		{
+			var wrapper = new BitmapWrapper (source);
+
+			effect.Render (wrapper);
+		}
+	}
+}
